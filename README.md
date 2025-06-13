ecf15b060188e84881ae1e6a845e13471f624159
LAWSON <98959131+Tamilawre@users.noreply.github.com>
2025-06-13 17:58:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# Boston-House-Prediction-Devtown-Bootcamp-Project" 
+This is my final project submission for the 5 day python zero-to-hero bootcamp hosted by DevTown.
+My task was to create a fully deployed linear regression model for predicting boston house prices with a minimal front end UI.
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [
+        float(request.form['CRIM']),
+        float(request.form['ZN']),
+        float(request.form['INDUS']),
+        float(request.form['CHAS']),
+        float(request.form['NOX']),
+        float(request.form['RM']),
+        float(request.form['AGE']),
+        float(request.form['DIS']),
+        float(request.form['RAD']),
+        float(request.form['TAX']),
+        float(request.form['PTRATIO']),
+        float(request.form['B']),
+        float(request.form['LSTAT']),
+    ]
+    np_features = np.array([features])
+    predicted = model.predict(np_features)
+    output = round(predicted[0], 2)
+
+    return render_template('index.html', prediction=f'The predicted price is {output}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
